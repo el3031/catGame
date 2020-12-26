44e1949bbe67770a0849708f39d012fd2c827b59
el3031 <67562724+el3031@users.noreply.github.com>
2020-12-25 23:22:55 -0800

    Initial commit

---
+++ Assets/Scripts/CatMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float maxSpeed = 3f;
+    bool facingLeft = true;
+    Animator anim;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector3(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        if (move < 0 && facingLeft || move > 0 && !facingLeft)
+        {
+            flip();
+        }
+    }
+
+    void flip()
+    {
+        facingLeft = !facingLeft;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
