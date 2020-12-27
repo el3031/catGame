5137bf9f0739e5de3f35240785773e0913de9a48
el3031 <67562724+el3031@users.noreply.github.com>
2020-12-27 15:35:33 -0800

    cat jumps, left-right animation fixed

--- Assets/Scripts/CatMovement.cs
+++ Assets/Scripts/CatMovement.cs
@@ -8,22 +8,46 @@ public class CatMovement : MonoBehaviour
     public float maxSpeed = 3f;
     bool facingLeft = true;
     Animator anim;
+
+    bool grounded = false;
+    public Transform groundDetect;
+    float groundRadius = 0.2f;
+    public LayerMask groundLayer;
+    public float jumpForce = 300f;
+
     
     void Start()
     {
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        //vertical motion
+        anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+        grounded = Physics2D.OverlapCircle(groundDetect.position, 
+                                           groundRadius, groundLayer);
+        anim.SetBool("Ground", grounded);
+        
+        //horizontal motion
         float move = Input.GetAxis("Horizontal");
-        GetComponent<Rigidbody2D>().velocity = new Vector3(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        GetComponent<Rigidbody2D>().velocity = new Vector3
+                    (move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
         anim.SetFloat("Speed", Mathf.Abs(move));
 
         if (move < 0 && facingLeft || move > 0 && !facingLeft)
         {
             flip();
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Ground", false);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
         }
     }
 
