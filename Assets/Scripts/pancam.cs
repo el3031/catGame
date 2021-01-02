0b5090b59ac6fa27863fed1c47d1e0b8fc86e9c3
el3031 <67562724+el3031@users.noreply.github.com>
2021-01-02 13:52:33 -0800

    pancam updated

--- Assets/Scripts/pancam.cs
+++ Assets/Scripts/pancam.cs
@@ -29,14 +29,17 @@ public class pancam : MonoBehaviour {
 			double startScroll = -5.5;
 			if (player.transform.position.x > startScroll) {
 
-				float randy = 0f;
-				randy = Random.Range (0f, 100f);
-				if (randy < 20) {
-					ydir = ydir + .005f;
-				} else if (randy > 20 && randy < 40) {
-					ydir = ydir - .005f;
-				} else if (randy > 80) {
-					ydir = 0f;
+				if (player.transform.position.y < Camera.main.orthographicSize / 2)
+				{
+					ydir = -Camera.main.orthographicSize / 4;
+				}
+				else if (player.transform.position.y > Camera.main.orthographicSize * 3 / 2)
+				{
+					ydir = Camera.main.orthographicSize / 4;
+				}
+				else
+				{
+					ydir = 0;
 				}
 
 				//panSpeed is how much we pan in the x-direction every time
