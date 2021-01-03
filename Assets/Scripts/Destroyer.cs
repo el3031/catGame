89b06b2bd31b1ab60d44f5e862d9b101c9d8a63a
el3031 <67562724+el3031@users.noreply.github.com>
2021-01-03 12:32:10 -0800

    destroyer code added, but the Destroy() still doesn't work yet

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Break();
+            return;
+        }
+        if (other.gameObject.transform.parent) 
+        {
+            Debug.Log("destroyed parent gameobject");
+            Destroy (other.gameObject.transform.parent.gameObject);
+        } 
+        else
+        {
+            Debug.Log("destroyed object");
+            Destroy (other.gameObject);		
+        }
+    }
+}
