515dbb57fe7998414eeb8c788bea04b480669a4b
el3031 <67562724+el3031@users.noreply.github.com>
2021-01-02 16:40:10 -0800

    whatever

--- Assets/Scripts/streetView.cs
+++ Assets/Scripts/streetView.cs
@@ -10,7 +10,7 @@ public class streetView : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        lastx = transform.position.x;
+        lastx = Camera.main.orthographicSize;
     }
 
     // Update is called once per frame
