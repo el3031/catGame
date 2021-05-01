9046d0314fa5a836a7cdb42e7eaf62fcf55cd3d5
el3031 <67562724+el3031@users.noreply.github.com>
2021-04-30 20:50:09 -0700

    very basic pause resume menu

---
+++ Assets/Scripts/PauseResume.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseResume : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool paused = false;
+    private Button pauseButton;
+    [SerializeField] private GameObject pauseMenu;
+    void Start()
+    {
+        pauseButton = GetComponent<Button>();
+    }
+
+    public void pause()
+    {
+        Time.timeScale = 0;
+        paused = true;
+        pauseMenu.SetActive(true);
+    }
+
+    public void resume()
+    {
+        Time.timeScale = 1;
+        paused = false;
+        pauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (paused)
+            {
+                resume();
+            }
+            else
+            {
+                pause();
+            }
+        }
+    }
+}
